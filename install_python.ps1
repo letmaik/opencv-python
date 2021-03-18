5be1a0242d34b487c31dceec9286580d7113e299
Maik Riechert <maik.riechert@arcor.de>
2021-03-18 21:43:20 +0000

    Update install_python.ps1

--- install_python.ps1
+++ install_python.ps1
@@ -1,10 +1,8 @@
 $exePath = "$env:TEMP\python-installer.exe"
 
-Write-Host "Downloading $env:PYTHON_URL ..."
+Write-Host "Downloading $env:PYTHON_URL"
 (New-Object Net.WebClient).DownloadFile($env:PYTHON_URL, $exePath)
 Write-Host "Installing..."
+Write-Host "cmd /c start /wait $exePath /quiet TargetDir=$env:PYTHON Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0 Include_debug=1"
 cmd /c start /wait $exePath /quiet TargetDir="$env:PYTHON" Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0 Include_debug=1
-Write-Host "Python ${req_ver} installed to $env:PYTHON"
 
-$py_exe = "${env:PYTHON}\Python.exe"
-echo "$(& $py_exe --version 2> $null)"
