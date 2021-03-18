3f0656b85a72a218ed6f3ef7d99ef63932a63ec0
Maik Riechert <maik.riechert@arcor.de>
2021-03-18 21:20:22 +0000

    Update install_python.ps1

--- install_python.ps1
+++ install_python.ps1
@@ -6,4 +6,5 @@ Write-Host "Installing..."
 cmd /c start /wait $exePath /quiet TargetDir="$env:PYTHON" Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0 Include_debug=1
 Write-Host "Python ${req_ver} installed to $env:PYTHON"
 
+$py_exe = "${env:PYTHON}\Python.exe"
 echo "$(& $py_exe --version 2> $null)"
