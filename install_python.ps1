8784b8beef890ab6afb3325dbee6d5d065d9693c
Maik Riechert <maik.riechert@arcor.de>
2021-03-18 21:45:20 +0000

    Update install_python.ps1

--- install_python.ps1
+++ install_python.ps1
@@ -6,3 +6,4 @@ Write-Host "Installing..."
 Write-Host "cmd /c start /wait $exePath /quiet TargetDir=$env:PYTHON Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0 Include_debug=1"
 cmd /c start /wait $exePath /quiet TargetDir="$env:PYTHON" Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0 Include_debug=1
 
+ls $env:PYTHON
